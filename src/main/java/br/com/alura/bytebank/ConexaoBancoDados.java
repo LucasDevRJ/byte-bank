b47828884ad5460cb69c3098a7cb149613be621b
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-07-21 16:57:46 -0300

    tratando o erro de conexão com o banco

--- src/main/java/br/com/alura/bytebank/ConexaoBancoDados.java
+++ src/main/java/br/com/alura/bytebank/ConexaoBancoDados.java
@@ -1,4 +1,15 @@
 package br.com.alura.bytebank;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexaoBancoDados {
+    public static void main(String[] args) {
+        try {
+            DriverManager
+                    .getConnection("jdbc:mysql://localhost:3306/byte_bank?user=root&password=root");
+        } catch (SQLException e) {
+            System.err.println("Erro: falha ao conectar com banco de dados!");
+        }
+    }
 }
