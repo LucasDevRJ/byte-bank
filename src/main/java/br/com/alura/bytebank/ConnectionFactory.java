43c55dd7a0ae1234e203189d34467f4355ecb83b
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-07-22 17:42:52 -0300

    limitando conexões com o banco

--- src/main/java/br/com/alura/bytebank/ConnectionFactory.java
+++ src/main/java/br/com/alura/bytebank/ConnectionFactory.java
@@ -1,5 +1,8 @@
 package br.com.alura.bytebank;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -7,10 +10,20 @@ import java.sql.SQLException;
 public class ConnectionFactory {
     public Connection recuperarConexao() {
         try {
-            return DriverManager
-                    .getConnection("jdbc:mysql://localhost:3306/byte_bank?user=root&password=root");
+            return criaFonteDados().getConnection();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    //método para limitar as conexões com o banco
+    private HikariDataSource criaFonteDados() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/byte_bank");
+        config.setUsername("root");
+        config.setPassword("root");
+        config.setMaximumPoolSize(10);
+
+        return new HikariDataSource(config);
     }
 }
